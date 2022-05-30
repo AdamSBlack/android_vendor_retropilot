7bc847e22cac6a5f5bf8e35552bd8f672a6ea176
Adam Black <adam@adamblack.us>
2022-05-30 22:57:52 +0100

    working??

--- retros/retros-vendor.mk
+++ retros/retros-vendor.mk
@@ -4,8 +4,6 @@
 $(warning $(LOCAL_PATH))
 
 PRODUCT_COPY_FILES += \
-    vendor/retropilot/retros/etc/permissions/privapp-permissions-retropilot.xml:system/etc/permissions/privapp-permissions-retropilot.xml \
-     vendor/retropilot/retros/priv-app/termux/termux-app_v0.118.0+github-debug_arm64-v8a.apk:system/priv-app/termux/termux-app_v0.118.0+github-debug_arm64-v8a.apk
-
+    vendor/retropilot/retros/etc/permissions/privapp-permissions-retropilot.xml:system/etc/permissions/privapp-permissions-retropilot.xml
 PRODUCT_PACKAGES += \
     termux
