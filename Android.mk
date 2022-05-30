94875ddbd933632b8957e01f1762dda1e9a0dbd4
Adam Black <18472768+AdamSBlack@users.noreply.github.com>
2022-05-30 17:04:55 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -30,6 +30,12 @@ LOCAL_MODULE_SUFFIX := .apk
 LOCAL_PRIVILEGED_MODULE := true
 include $(BUILD_PREBUILT)
 
+PRODUCT_COPY_FILES += \
+    vendor/retropilot/etc/permissions/privapp-permissions-retropilot.xml:system/etc/permissions/privapp-permissions-retropilot.xml \
+     vendor/retropilot/priv-app/termux/termux-app_v0.118.0+github-debug_arm64-v8a.apk:system/priv-app/termux/termux-app_v0.118.0+github-debug_arm64-v8a.apk
+
+PRODUCT_PACKAGES += \
+    termux
 
 
 endif
