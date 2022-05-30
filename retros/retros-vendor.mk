da0e14397299790230898c38402489a26042231b
Adam Black <adam@adamblack.us>
2022-05-30 23:38:47 +0100

    fix

--- retros/retros-vendor.mk
+++ retros/retros-vendor.mk
@@ -1,8 +1,3 @@
-
-
-@echo "anger"
-$(warning $(LOCAL_PATH))
-
 PRODUCT_COPY_FILES += \
     vendor/retropilot/retros/etc/permissions/privapp-permissions-retropilot.xml:system/etc/permissions/privapp-permissions-retropilot.xml
 PRODUCT_PACKAGES += \
