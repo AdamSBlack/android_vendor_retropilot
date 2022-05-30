a2cd8037b534fdbc79863a0a52b8f07170307c8d
Adam Black <adam@adamblack.us>
2022-05-30 18:44:22 +0100

    missing file

---
+++ retros/retros-vendor.mk
@@ -0,0 +1,7 @@
+
+PRODUCT_COPY_FILES += \
+    vendor/retropilot/retros/etc/permissions/privapp-permissions-retropilot.xml:system/etc/permissions/privapp-permissions-retropilot.xml \
+     vendor/retropilot/retros/priv-app/termux/termux-app_v0.118.0+github-debug_arm64-v8a.apk:system/priv-app/termux/termux-app_v0.118.0+github-debug_arm64-v8a.apk
+
+PRODUCT_PACKAGES += \
+    termux
