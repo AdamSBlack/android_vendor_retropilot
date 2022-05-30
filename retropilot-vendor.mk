fcd262a2da21c4a3b3cf4d463874765750325434
Adam Black <adam@adamblack.us>
2022-05-30 18:24:27 +0100

    forgotten files

--- retropilot-vendor.mk
+++
@@ -1,7 +0,0 @@
-
-PRODUCT_COPY_FILES += \
-    vendor/retropilot/etc/permissions/privapp-permissions-retropilot.xml:system/etc/permissions/privapp-permissions-retropilot.xml \
-     vendor/retropilot/priv-app/termux/termux-app_v0.118.0+github-debug_arm64-v8a.apk:system/priv-app/termux/termux-app_v0.118.0+github-debug_arm64-v8a.apk
-
-PRODUCT_PACKAGES += \
-    termux
