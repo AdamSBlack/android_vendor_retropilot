9e88ff57a2dd90b2a6bdcbbea854df1cc93f1ada
Adam Black <adam@adamblack.us>
2022-05-31 20:17:47 +0100

    added vendor bits

--- retros/retros-vendor.mk
+++ retros/retros-vendor.mk
@@ -1,4 +1,5 @@
 PRODUCT_COPY_FILES += \
-    vendor/retropilot/retros/etc/permissions/privapp-permissions-retropilot.xml:system/etc/permissions/privapp-permissions-retropilot.xml
+    vendor/retropilot/retros/etc/permissions/privapp-permissions-retropilot.xml:system/etc/permissions/privapp-permissions-retropilot.xml \
+    vendor/retropilot/retros/file-store/userspace.tar.gz:system/etc/retros/userspace.tar.gz
 PRODUCT_PACKAGES += \
     termux
