16d97aeb91018c281201d703ad66e1e17b1e443f
Adam Black <adam@adamblack.us>
2022-06-01 02:03:27 +0100

    test

--- retros/retros-vendor.mk
+++ retros/retros-vendor.mk
@@ -1,6 +1,6 @@
 PRODUCT_COPY_FILES += \
     vendor/retropilot/retros/etc/permissions/privapp-permissions-retropilot.xml:system/etc/permissions/privapp-permissions-retropilot.xml \
     vendor/retropilot/retros/etc/retros/userspace.tar.gz:system/etc/retros/userspace.tar.gz \
-    vendor/retropilot/retros/etc/retros/init.retros.rc:system/etc/retros/init.retros.rc
+    vendor/retropilot/retros/etc/retros/init.retros.rc:system/etc/init/init.retros.rc
 PRODUCT_PACKAGES += \
     termux
