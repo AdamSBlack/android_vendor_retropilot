986375c5f900d2e224b762a99162e11b977e2b8b
Adam Black <18472768+AdamSBlack@users.noreply.github.com>
2022-06-23 18:25:57 +0100

    include bootanim in make

--- retros/retros-vendor.mk
+++ retros/retros-vendor.mk
@@ -6,9 +6,11 @@ PRODUCT_COPY_FILES += \
     vendor/retropilot/retros/bin/retros_launch.sh:system/bin/retros_launch.sh \
     vendor/retropilot/retros/bin/retros_userspace.sh:system/bin/retros_userspace.sh \
     vendor/retropilot/retros/bin/retros_sshd.sh:system/bin/retros_sshd.sh \
-        vendor/retropilot/retros/bin/retros_android_settings.sh:system/bin/retros_android_settings.sh \
+    vendor/retropilot/retros/bin/retros_android_settings.sh:system/bin/retros_android_settings.sh \
     vendor/retropilot/retros/bin/busybox:system/bin/busybox \
-    vendor/retropilot/retros/xbin/su:system/xbin/su 
+    vendor/retropilot/retros/xbin/su:system/xbin/su \
+    vendor/retropilot/retros/bootanimation.zip:system/media/bootanimation.zip 
+
     
 PRODUCT_PACKAGES += \
     termux
